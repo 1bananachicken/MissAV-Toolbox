f734f91c3e6d5eff1202e6ff9fd923fcb19401a4
Genshin Impact，Launch！ <116740703+1bananachicken@users.noreply.github.com>
2024-08-30 17:50:27 +0800

    version 0.1

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='missav_utils',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'cloudscraper',
+        'Pillow',
+        'bs4',
+    ],
+    include_package_data=True,
+    description='A brief description of my project',
+    author='bananachicken',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
