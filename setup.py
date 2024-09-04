21adceb94f866c95852c7d6954c00e9a2486cb5c
Genshin Impact，Launch！ <2896318607@qq.com>
2024-09-04 13:45:32 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
         'ffmpeg-python',
     ],
     long_description=long_description,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     author='bananachicken',
     classifiers=[
