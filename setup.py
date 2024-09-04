76d7f7f595ec3d7d7ec6e720c1fe87a95ccb7e16
Genshin Impact，Launch！ <116740703+1bananachicken@users.noreply.github.com>
2024-09-04 18:02:51 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='missav_toolbox',
-    version='1.0.0',
+    version='1.1.0',
     url='https://github.com/1bananachicken/MissAV-Toolbox',
     packages=find_packages(),
     install_requires=[
